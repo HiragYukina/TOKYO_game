3338235178d7105e0d21ced00b1d4c455ca433d0
yukina <g.taiki.0909@gmail.com>
2018-04-17 17:04:50 +0900

    タイルマップ

--- game.js
+++ game.js
@@ -1,49 +1,39 @@
-var speed = 3;
-var x = width / 3,
-  y = height / 2;
-var b = 0,
-  s = 0;
-var keystate = {
-  "w": false,
-  "d": false,
-  "s": false,
-  "a": false,
-  "e": false
-}
-function gameRender() {}
-//プレイヤー移動
-function gameKey() {
-  if (keystate["w"]) {
-    player.position.y += 2;
+//キー
+let keystate = {
+    "a": false,
+    "d": false,
+    "x": false,
+    "s": false
   }
-  if (keystate["s"]) {
-    player.position.y -= 2;
-  }
-  if (keystate["d"]) {
-    mainUsa.alpha = 0.5;
-    ase.alpha = 0.5;
-    player.position.x -= 2;
-  }
-  if (keystate["a"]) {
-    player.position.x += 2;
-  }
+  //プレイヤー
+  let playerImages,
+    player = {
+      x: 0,
+      y: 0
+    };
 
-  if (keystate["d"] == false) {
-    if (mainUsa) {
-      mainUsa.alpha = 1;
+  //プレイヤーの描画
+  //function renderPlayer() {}
+  //キー入力
+  function playerKey() {
+    if (keystate["a"]) {
+      camera.x--
+    }
+    if (keystate["w"]) {
+      camera.y--
+    }
+    if (keystate["d"]) {
+      camera.x++
+    }
+    if (keystate["s"]) {
+      camera.y++
     }
   }
-  if (keystate["e"]) {
-    stage.addChild(hage);
-    stage.removeChild(mainUsa);
-  }
 
-}
-
-//キーイベント
-window.addEventListener("keydown", function(e) {
-  keystate[e.key] = true;
-});
-window.addEventListener("keyup", function(e) {
-  keystate[e.key] = false;
-});
+  //キーイベント
+  window.addEventListener("keydown", function(e) {
+    keystate[e.key] = true;
+  })
+  window.addEventListener("keyup", function(e) {
+    keystate[e.key] = false;
+  })
--- main.js
+++ main.js
@@ -1,161 +1,100 @@
-var gameMode = "game"
-  var usa,
-    mainUsa,
-    ground,
-    ase,
-    hage;
-  var layers,
-    map_width,
-    map_height,
-    tile_rect_w,
-    tile_rect_h;
-  var width = 800,
-    height = 450;
-  var x0 = -700;
-  var y0 = -900;
-  var plyer;
-  var score;
-  var title;
-  var renderOption = {
-    antialiasing: false,
-    transparent: false,
-    resolution: window.devicePixeRatio,
-    autoResize: true,
-    backgroundColor: 0xffffff
-  }
-  var renderer = PIXI.autoDetectRenderer(width, height, renderOption);
-  renderer.view.style.position = "absolute";
-  renderer.view.style.top = "0px";
-  renderer.view.style.left = "0px";
+//HTML読み込み完了
+window.addEventListener("DOMContentLoaded", init)
 
-  var stage = new PIXI.Container();
+//初期化
+function init() {
+  //キャンパス要素
+  canvas = document.getElementById("maincanvas")
+  ctx = canvas.getContext("2d")
+  //マップタイル読み込み
+  image = new Image()
+  image.src = "images/map1.png"
+  image.addEventListener("load", function() {
+    //chromeのバグ回避
+    let backcanvas = document.createElement("canvas")
+    backcanvas.width = 960
+    backcanvas.height = 720
+    const backctx = backcanvas.getContext("2d")
+    backctx.drawImage(image, 0, 0)
+    image = backcanvas
+  })
 
-  window.addEventListener("DOMContentLoaded", init)
-  document.body.appendChild(renderer.view)
-  window.addEventListener("resize", resize);
+  //マップ
+  //読み込み
+  mapload(function(event) {
+    loadmap(event);
+    requestAnimationFrame(updata)
+  })
+}
 
-  function init() {
-    PIXI.loader.add("images/usa.png").add("images/hapi_hu.png").add("images/hapi_hazusi.png").add("images/hapirabi100.png").add("images/hapirabi75.png").add("images/hapirabi50.png").add("images/hapirabi25.png").add("images/kodomo.png").add("images/kodomo_hu.png").add("images/otona.png").add("images/map1.png").add("images/ase.png").add("images/title.png").add("images/start.png").add("images/stop.png").add("images/hapirabi_map.png").load(setup);
-    resize();
-    //JSONロード
-    (function(loadingMap) {
-      var map = new XMLHttpRequest;
+function mapload(onload) {
+  //JSON読み込み
+  var map = new XMLHttpRequest
 
-      map.addEventListener("load", loadingMap, false);
-      map.open("GET", "images/hapirabi_map.json", true);
-      map.send(null);
-    }(function loadingMap(event) {
-      var loadmap = event.target;
-      var map = JSON.parse(loadmap.responseText);
-      //stage抽出
-      layers = map.layers;
+  map.addEventListener("load", onload, false)
+  map.open("GET", "images/hapirabi_map.json", true)
+  map.send(null)
+}
+function loadmap(event) {
+  var loadmap = event.target
+  var mainmap = JSON.parse(loadmap.responseText)
 
-      //マップの横幅
-      map_width = map.width;
-      //マップの高さ
-      map_height = map.height;
-      //タイルの幅
-      tile_rect_w = map.tilewidth;
-      //タイルの高さ
-      tile_rect_h = map.tileheight;
+  //レイヤー抽出
+  layers = mainmap.layers
+  //マップの横幅
+  map.width = mainmap.width
+  //マップ高さ
+  map.height = mainmap.height
+  //タイル
+  tile.width = mainmap.tilewidth
+  tile.height = mainmap.tileheight
 
-    }));
-    requestAnimationFrame(animate);
-  }
-  //ステージ生成
-  function stageinit() {
-    var map_id = [0];
-    for (var i = 0; i < layers.length; i++) {
-      var map = layers[i];
-      for (var y = 0; y < 100; y++) {
-        for (var x = 0; x < 100; x++) {
-          var j = x + (y * 100);
-          //サイズ(48,48)
-          var tile = [tile_rect_w, tile_rect_h];
-          var map.data[j]
-        }
+  //マップの最大・最小
+  widthMax = map.width * tile.width
+  heightMax = map.height * tile.height
+}
+
+function renderMap() {
+
+  const screenLeft = camera.x - screenWidth / 2
+  const screenTop = camera.y - screenHeight / 2
+  //描画開始位置
+  const startTileX = Math.floor(screenLeft / tile.width)
+  const startTiley = Math.floor(screenTop / tile.height)
+  //タイル数
+  const tileX = screenWidth / tile.width
+  const tileY = screenHeight / tile.height
+  for (var i = 0; i < layers.length; i++) {
+    for (var y = 0; y < tileY; y++) {
+      for (var x = 0; x < tileX; x++) {
+        const index = startTileX + x + (startTiley + y) * map.width
+
+        let id = layers[i].data[index] - 1
+        let sx = (id % 20) * tile.width
+        let sy = Math.floor(id / 15) * tile.height
+        const dx = x * tile.width - screenLeft % tile.width
+        const dy = y * tile.height - screenTop % tile.height
+        //         タイル画像、トリミング　ｘ、ｙ、  タイル幅ｘ、ｙ、描画開始位置　ｘ、ｙ　タイル幅　　　　　　　描画タイルｘ、ｙ、
+        ctx.drawImage(image, sx, sy, tile.width, tile.height, dx, dy, tile.width, tile.height);
+
       }
     }
-
-  }
-  //画像読み込み
-  function setup() {
-    player = new PIXI.Sprite(PIXI.loader.resources["images/hapirabi_map.png"].texture);
-    player.position.set(x0, y0)
-
-    var hageusa = PIXI.utils.TextureCache["images/hapi_hazusi.png"];
-    var hagerect = new PIXI.Rectangle(270, 0, 270, 450);
-    hageusa.frame = hagerect;
-    hage = new PIXI.Sprite(hageusa);
-    hage.position.set(520, 0);
-    //大きいうさぎ
-    var B_usa = PIXI.utils.TextureCache["images/hapirabi100.png"];
-    var rect = new PIXI.Rectangle(0, 0, 270, 450);
-    B_usa.frame = rect;
-    mainUsa = new PIXI.Sprite(B_usa);
-    mainUsa.position.set(520, 0);
-    //プレイヤー
-    //var player = PIXI.utils.TextureCache["images/usa.png"];
-    var frames = [];
-    var pattern = [0, 1, 0, 2];
-    for (var i = 0; i < pattern.length; i++) {
-      var usarect = new PIXI.Rectangle(pattern[i] * 48, 0, 48, 48);
-      var texture = new PIXI.Texture(PIXI.utils.TextureCache["images/usa.png"]);
-      texture.frame = usarect;
-      frames.push(texture);
-    }
-    usa = new PIXI.extras.AnimatedSprite(frames);
-    usa.animationSpeed = 0.1;
-    usa.play();
-    usa.position.set(x, y);
-
-    //汗
-    var a = PIXI.utils.TextureCache["images/ase.png"];
-    var arect = new PIXI.Rectangle(0, 0, 270, 450);
-    a.frame = arect;
-    ase = new PIXI.Sprite(a);
-    ase.position.set(520, 0);
-
-    //スコア
-    score = new PIXI.Text(b, {fill: "#000000"});
-    score.position.x = 0;
-    score.position.y = 10;
-
-    title = new PIXI.Sprite(PIXI.loader.resources["images/title.png"].texture);
-    stage.addChild(player);
-    stage.addChild(usa);
-    stage.addChild(mainUsa);
-    stage.addChild(ase);
-
-    stageinit();
   }
 
-  function animate() {
-    requestAnimationFrame(animate);
-    render();
-    renderer.render(stage);
+}
+
+function updata() {
+  requestAnimationFrame(updata)
+  render()
+}
+
+function render() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  switch (gamemode) {
+    case "game":
+      renderMap();
+      playerKey();
+      break;
   }
 
-  function render() {
-
-    switch (gameMode) {
-      case "title":
-        titleRender();
-        break;
-      case "game":
-        gameRender();
-        gameKey();
-        //  gamestag();
-        break;
-      case "end":
-        endRender();
-        break;
-    }
-  }
-  function resize() {
-    ratio = Math.min(window.innerWidth / width, window.innerHeight / height);
-
-    stage.scale.x = stage.scale.y = ratio;
-
-    renderer.resize(Math.ceil(width * ratio), Math.ceil(height * ratio));
-  }
+}
