345c44de9cce0a2263f253e261e296c0e5e4df0c
Kazuki Miyanishi <zukkun@gmail.com>
2018-04-26 17:27:54 +0900

    テスト用のカメラ移動処理

--- src/main.js
+++ src/main.js
@@ -25,6 +25,20 @@ window.addEventListener("DOMContentLoaded", init)
  */
 function update() {
   requestAnimationFrame(update)
+
+  // テスト用のカーソルキー入力によるカメラ移動処理
+  if (keyState.up) {
+    camera.y -= 10
+  } else if (keyState.down) {
+    camera.y += 10
+  }
+  if (keyState.left) {
+    camera.x -= 10
+  } else if (keyState.right) {
+    camera.x += 10
+  }
+
+  // 描画
   render()
 }
 
@@ -36,9 +50,52 @@ function render() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   switch (gamemode) {
     case "game":
-      tilemap.render(ctx, {x: 0, y: 0})
+      tilemap.render(ctx, camera)
       playerKey();
       break;
   }
 
 }
+
+
+/**
+ * 簡易的なカーソルキーの処理
+ */
+var keyState = {
+  up: false,
+  down: false,
+  left: false,
+  right: false
+}
+addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ArrowUp":
+      keyState.up = true
+      break
+    case "ArrowDown":
+      keyState.down = true
+      break
+    case "ArrowLeft":
+      keyState.left = true
+      break
+    case "ArrowRight":
+      keyState.right = true
+      break
+  } 
+})
+addEventListener("keyup", (e) => {
+  switch (e.key) {
+    case "ArrowUp":
+      keyState.up = false 
+      break
+    case "ArrowDown":
+      keyState.down = false
+      break
+    case "ArrowLeft":
+      keyState.left = false
+      break
+    case "ArrowRight":
+      keyState.right = false
+      break
+  } 
+})
